1ea9f6412ef2cd929b1f7eba3e0ff7601a39386f
Saran <saranrajmalar@gmail.com>
2022-01-21 18:11:22 +0530

    implement remove from cart functionality

--- problem1.js
+++ problem1.js
@@ -1,10 +1,22 @@
 const shoppingCart = [];
 
-function addToCart(product) {
-    shoppingCart.push(product)
+function addToCart(product, quantity) {
+    const cart = {
+        product, quantity
+    }
+    return shoppingCart.push(cart)
+}
+
+function removeFromCart(product) {
+    let cartIndex = shoppingCart.findIndex((item) => {
+        return item.product === product;
+    });
+
+    return shoppingCart.splice(cartIndex, 1)
 }
 
 module.exports = {
     addToCart,
-    shoppingCart
+    shoppingCart,
+    removeFromCart
 }
--- problem1.test.js
+++ problem1.test.js
@@ -1,13 +1,23 @@
-const { addToCart, shoppingCart } = require("./problem1.js")
+const { addToCart, shoppingCart, removeFromCart } = require("./problem1.js")
 
 describe("Shopping cart", () => {
-    test('Adding IPad to cart', () => {
-        addToCart('IPad')
-        expect(shoppingCart).toEqual(['IPad'])
+    test('Adding IPad with quantity to cart', () => {
+        addToCart('IPad', 1)
+        expect(shoppingCart).toEqual([{"product": "IPad", "quantity": 1}])
     })
 
     test('Adding Hero ink Pen to cart', () => {
-        addToCart('Hero ink Pen')
-        expect(shoppingCart).toEqual(['IPad','Hero ink Pen'])
+        addToCart('Hero ink Pen', 1)
+        expect(shoppingCart).toEqual([{"product": "IPad", "quantity": 1}, {"product": "Hero ink Pen", "quantity": 1}])
+    })
+
+    test('Adding 2 quantity GM Cricket bat to cart', () => {
+        addToCart('GM Cricket bat', 2)
+        expect(shoppingCart).toEqual([{"product": "IPad", "quantity": 1}, {"product": "Hero ink Pen", "quantity": 1},{"product": "GM Cricket bat", "quantity": 2}])
+    })
+
+    test('remove IPad from cart', () => {
+        removeFromCart('IPad')
+        expect(shoppingCart).toEqual([{"product": "Hero ink Pen", "quantity": 1},{"product": "GM Cricket bat", "quantity": 2}])
     })
 })
