c6475157f519083400444770c58d65b1a446d19e
Abhinav Gupta <abhinav.gupta@thoughtworks.com>
2022-01-21 15:35:17 +0530

    Added remove & checkout as well.

--- problem1.js
+++ problem1.js
@@ -4,4 +4,12 @@ function addToCart(product) {
     shoppingCart.push(product)
 }
 
+function removeFromCart(product) {
+    shoppingCart.pop(product)
+}
+
+function checkoutCart(product) {
+    //Checkout logic
+}
+
 addToCart("IPad Pro");
